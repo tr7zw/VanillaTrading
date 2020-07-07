56fcd84671bac7e532b570d709544dc44156b47d
tr7zw <tr7zw@live.de>
2020-07-07 17:32:28 +0200

    Add 1.14 support

---
+++ NMS1_14_R1/src/main/java/de/tr7zw/vanillatrading/nms/v15_R1/MerchantGuiListener14R1.java
@@ -0,0 +1,23 @@
+package de.tr7zw.vanillatrading.nms.v15_R1;
+
+import org.bukkit.craftbukkit.v1_14_R1.inventory.CraftInventoryMerchant;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+import de.tr7zw.vanillatrading.nms.ShopMerchant;
+
+public class MerchantGuiListener14R1 implements Listener {
+
+	@EventHandler
+	public void onClose(InventoryCloseEvent event) {
+		if (event.getInventory() instanceof CraftInventoryMerchant) {
+			CraftInventoryMerchant merchantInv = (CraftInventoryMerchant) event.getInventory();
+			if (merchantInv.getMerchant() instanceof ShopMerchant) {
+				ShopMerchant merchant = (ShopMerchant) merchantInv.getMerchant();
+				merchant.getShop().onShopClose(event.getPlayer());
+			}
+		}
+	}
+
+}
--- VanillaTrading/src/main/java/de/tr7zw/vanillatrading/nms/NMSHandler.java
+++ VanillaTrading/src/main/java/de/tr7zw/vanillatrading/nms/NMSHandler.java
@@ -4,7 +4,9 @@ import org.bukkit.Bukkit;
 
 import de.tr7zw.nbtapi.utils.MinecraftVersion;
 import de.tr7zw.vanillatrading.VanillaTrading;
+import de.tr7zw.vanillatrading.nms.v15_R1.MerchantGuiListener14R1;
 import de.tr7zw.vanillatrading.nms.v15_R1.MerchantGuiListener15R1;
+import de.tr7zw.vanillatrading.nms.v15_R1.NMS14R1;
 import de.tr7zw.vanillatrading.nms.v15_R1.NMS15R1;
 import de.tr7zw.vanillatrading.nms.v16_R1.MerchantGuiListener16R1;
 import de.tr7zw.vanillatrading.nms.v16_R1.NMS16R1;
@@ -15,6 +17,10 @@ public class NMSHandler {
 	
 	static {
 		MinecraftVersion version = MinecraftVersion.getVersion();
+		if(version == MinecraftVersion.MC1_14_R1) {
+			nmsImplementation = new NMS14R1();
+			Bukkit.getPluginManager().registerEvents(new MerchantGuiListener14R1(), VanillaTrading.INSTANCE);
+		}
 		if(version == MinecraftVersion.MC1_15_R1) {
 			nmsImplementation = new NMS15R1();
 			Bukkit.getPluginManager().registerEvents(new MerchantGuiListener15R1(), VanillaTrading.INSTANCE);
